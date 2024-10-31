d430c368a992f80d00582a6875827bef479eb01c
ATTTAUFIQ <workattartaufiq@gmail.com>
2024-10-31 17:12:06 +0530

    02_variables.js Varibale study

---
+++ 02_variables.js
@@ -0,0 +1,21 @@
+const accountId=1233445;
+let email="worktaufiqattar@gmail.com"
+var password=123
+name="Taufiq"
+ 
+
+//print data on console
+console.log(accountId,email,password,name)
+
+//to print the data in the tabular format 
+console.table([accountId,email,password,name])
+
+
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+// The let and const keywords were added to JavaScript in 2015.
+// The var keyword should only be used in code written for older browsers.
+
+
+//var has problem in block scope and can be redeclared and reinitialized
+//let can be reinitialized but not redeclared
+//const cannot be reinitialized or redeclared
