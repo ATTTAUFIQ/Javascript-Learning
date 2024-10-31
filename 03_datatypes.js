7ee5a0b67d6bf591380c71a23ff3747170d823c0
ATTTAUFIQ <workattartaufiq@gmail.com>
2024-10-31 17:32:56 +0530

    datatypes

---
+++ 03_datatypes.js
@@ -0,0 +1,17 @@
+//datatypes
+
+const name="taufiq"
+const age =20
+const isMarried=false
+const nulll=null
+let afterSomeTime;
+
+console.log(typeof name) //string
+console.log(typeof age) //number
+console.log(typeof isMarried) //boolean
+console.log(typeof nulll) //object
+console.log(typeof afterSomeTime) //undefined
+
+//more types 
+//1.bigint
+//2.Object
