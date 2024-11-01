ae5522eac972f4a8c3c6a95d7974a17cddbd3558
ATTTAUFIQ <workattartaufiq@gmail.com>
2024-11-01 11:45:25 +0530

    Strings in JS

---
+++ 05_stringsInJs.js
@@ -0,0 +1,20 @@
+//String are immutable in javaScript
+
+//ways to declare string in js
+const fisrtName=new String("Attar")
+const name="taufiq"
+console.table([fisrtName,name])
+
+
+//Accesing string based on indexes
+console.log(name[2]) //op->u
+
+
+//Converting string to array
+console.log(name.split('a')) //op->[ 't', 'ufiq' ]
+
+//varoius methods in JS
+console.log(name.indexOf('a'))
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.repeat(3))
